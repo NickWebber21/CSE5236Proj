65590d9be0ea185b67266d8f1d12821a9905128e
nick webber <nicholaswebbercse@outlook.com>
2024-10-16 16:10:05 -0400

    switched login fragment to handle button clicks with onClickListener\nimplemented onClickListner for login and create buttons\nremoved some unused code that was commented out\nalso uncommented onCreate so that we can pass some arguments via a Bundle or if we need some initialization tasks before the UI is created

--- app/src/main/java/com/example/schedulemaster/LoginFragment.kt
+++ app/src/main/java/com/example/schedulemaster/LoginFragment.kt
@@ -10,83 +10,66 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-/*private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"*/
-
 /**
  * A simple [Fragment] subclass.
  * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class LoginFragment : Fragment(), View.OnClickListener {
     private lateinit var mEditUsernameText: EditText
     private lateinit var mEditPasswordText: EditText
     private lateinit var mLoginButton: Button
     private lateinit var mCreateAccountButton: Button
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
+        // dont have any params at the moment
+
+        /*arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-        }
-    }*/
+        }*/
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val v: View
+    ): View {
+        val v = inflater.inflate(R.layout.fragment_login, container, false) // view
         val tag = "OnCreateView"
         Log.i(tag, "-----------------------NEW LOG---------------------")
         Log.i(tag, "onCreateView fragment started.")
-        // Inflate the layout for this fragment
-        v = inflater.inflate(R.layout.fragment_login, container, false)
 
         // references to the ui elements specified in XML
         mEditUsernameText = v.findViewById(R.id.usernameText)
         mEditPasswordText = v.findViewById(R.id.passwordText)
         mLoginButton = v.findViewById(R.id.loginButton)
-        mCreateAccountButton = v.findViewById(R.id.createAccountButton)
+        mLoginButton.setOnClickListener(this)
+        mCreateAccountButton = v.findViewById(R.id.createAccountButton) // wip
+        mCreateAccountButton.setOnClickListener(this)
 
-        // on click listener for login button using lambda
-        mLoginButton.setOnClickListener {
-            val username = mEditUsernameText.text.toString().trim()
-            val password = mEditPasswordText.text.toString().trim()
-
-            // for later: might want to check if valid username and password
-            // login action
-            Log.d("INSIDE LoginFragment.kt", "logging in with the username entered: $username")
-            Toast.makeText(requireContext(), "login successful!!!", Toast.LENGTH_SHORT).show()
-        }
-
-        // add create account button listener HERE
-
-        return view
+        return v
     }
 
-    /*companion object {
-        *//**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         *//*
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.loginButton -> {
+                // do stuff with FireBase when login button is clicked
+                val username = mEditUsernameText.text.toString().trim()
+                val password = mEditPasswordText.text.toString().trim()
+                Log.d("INSIDE LoginFragment.kt", "logging in with the username entered: $username and password: $password")
+                Toast.makeText(requireContext(), "login button clicked", Toast.LENGTH_SHORT).show()
             }
-    }*/
+            R.id.createAccountButton -> {
+                // do stuff with FireBase for when create account button is clicked
+                val username = mEditUsernameText.text.toString().trim()
+                val password = mEditPasswordText.text.toString().trim()
+                Log.d("INSIDE LoginFragment.kt", "creating account with the username entered: $username and password: $password")
+                Toast.makeText(requireContext(), "create account button clicked", Toast.LENGTH_SHORT).show()
+
+            }
+            else -> Log.e("INSIDE LoginFragment.kit", "Error: Invalid button press")
+        }
+    }
 }
