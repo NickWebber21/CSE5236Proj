e0a62a687d68191d220484ffeee592712d98201f
Ian Kwiatkowski <iankwiatko@gmail.com>
2024-10-16 20:37:25 -0400

    added new page for data CRUD display

--- app/src/main/java/com/example/schedulemaster/CreateAccountFragment.kt
+++ app/src/main/java/com/example/schedulemaster/CreateAccountFragment.kt
@@ -1,10 +1,16 @@
 package com.example.schedulemaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +22,43 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CreateAccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CreateAccountFragment : Fragment() {
+class CreateAccountFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var mEditUsernameText: EditText
+    private lateinit var mEditPasswordText: EditText
+    private lateinit var mCreateAccountButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        //currently no params
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
-    }
+    ): View {
+        val v = inflater.inflate(R.layout.fragment_create_account, container, false) // view
+        val tag = "OnCreateView"
+        Log.i(tag, "-----------------------NEW LOG---------------------")
+        Log.i(tag, "onCreateView fragment started.")
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateAccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateAccountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // references to the ui elements specified in XML
+        mEditUsernameText = v.findViewById(R.id.usernameEditText)
+        mEditPasswordText = v.findViewById(R.id.passwordEditText)
+        mCreateAccountButton = v.findViewById(R.id.createAccountButton) // wip
+        mCreateAccountButton.setOnClickListener(this)
+
+        return v;
+    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.createAccountButton -> {
+                // do stuff with FireBase for when create account button is clicked
+                Log.d("INSIDE CreateAccount.kt", "creating account")
+                Toast.makeText(requireContext(), "create account button clicked", Toast.LENGTH_SHORT).show()
             }
+            else -> Log.e("INSIDE LoginFragment.kit", "Error: Invalid button press")
+        }
     }
 }
