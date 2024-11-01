d5f43f46bfdaed6617bd8bb40f47c8331f0e5443
nick webber <nicholaswebbercse@outlook.com>
2024-11-01 16:17:37 -0400

    finished cleaning up activities and now they extend singlefragmentactivity and work now adding lifecycle methods to singlefragmentactivity

--- app/src/main/java/com/example/schedulemaster/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/schedulemaster/ui/activity/LoginActivity.kt
@@ -1,30 +1,10 @@
 package com.example.schedulemaster.ui.activity
 
-import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.schedulemaster.ui.fragment.LoginFragment
-import com.example.schedulemaster.R
 
 class LoginActivity : SingleFragmentActivity() {
     override fun createFragment(): Fragment {
         return LoginFragment()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        val tag = "OnCreate"
-        Log.i(tag, "-----------------------NEW LOG---------------------")
-        Log.i(tag, "onCreate activity started.")
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
-        val fm = supportFragmentManager // get instance of fragment manager to create fragment transaction
-        var fragment = fm.findFragmentById(R.id.fragment_container)
-        // add login fragment to container
-        if (fragment == null) {
-            fragment = LoginFragment()
-            fm.beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
-        }
     }
 }
--- app/src/main/java/com/example/schedulemaster/ui/activity/SingleFragmentActivity.kt
+++ app/src/main/java/com/example/schedulemaster/ui/activity/SingleFragmentActivity.kt
@@ -1,6 +1,7 @@
 package com.example.schedulemaster.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -15,6 +16,9 @@ abstract class SingleFragmentActivity : AppCompatActivity() {
         get() = R.layout.activity_fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val tag = "OnCreate"
+        Log.i(tag, "-----------------------NEW LOG---------------------")
+        Log.i(tag, "onCreate activity started.")
         super.onCreate(savedInstanceState)
         setContentView(layoutResId)
 
