8de098663611d81e395fda2b51db34b8a56a018e
nick webber <nicholaswebbercse@outlook.com>
2024-11-01 16:49:50 -0400

    added all lifecycle methods and transient lifecycle methods to singlefragmentactivity

--- app/src/main/java/com/example/schedulemaster/ui/activity/SingleFragmentActivity.kt
+++ app/src/main/java/com/example/schedulemaster/ui/activity/SingleFragmentActivity.kt
@@ -16,9 +16,7 @@ abstract class SingleFragmentActivity : AppCompatActivity() {
         get() = R.layout.activity_fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val tag = "OnCreate"
-        Log.i(tag, "-----------------------NEW LOG---------------------")
-        Log.i(tag, "onCreate activity started.")
+        logLifecycleMethod("onCreate")
         super.onCreate(savedInstanceState)
         setContentView(layoutResId)
 
@@ -32,5 +30,55 @@ abstract class SingleFragmentActivity : AppCompatActivity() {
                 .commit()
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        logLifecycleMethod("onStart")
+    }
+
+    // transient state
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        logLifecycleMethod("onRestoreInstanceState")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logLifecycleMethod("onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logLifecycleMethod("onResume")
+    }
+
+    // transient state
+    override fun onSaveInstanceState(outState: Bundle) {
+        logLifecycleMethod("onSaveInstanceState")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onPause() {
+        logLifecycleMethod("onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logLifecycleMethod("onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logLifecycleMethod("onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logLifecycleMethod(event: String) {
+        val activityName = javaClass.simpleName
+        Log.i("SingleFragmentActivity Lifecycle Methods", "-----------------------NEW LOG---------------------")
+        Log.i("SingleFragmentActivity Lifecycle Methods", "$activityName activity started. LifeCycleMethod: $event")
+    }
+
+
 
 }
