62d29ed1c9c19ab66d3592a984c20e36422bb60c
Nick Webber <nicholaswebbercse@outlook.com>
2024-11-03 15:45:07 -0500

    added home page fragment and activity need to implement logic and update manifest

--- app/src/main/java/com/example/schedulemaster/ui/activity/WelcomeActivity.kt
+++ app/src/main/java/com/example/schedulemaster/ui/activity/WelcomeActivity.kt
@@ -1,10 +1,9 @@
 package com.example.schedulemaster.ui.activity
 
 import androidx.fragment.app.Fragment
-import com.example.schedulemaster.ui.fragment.LoginFragment
 import com.example.schedulemaster.ui.fragment.WelcomeFragment
 
-class WelcomeActivity : SingleFragmentActivity {
+class WelcomeActivity : SingleFragmentActivity() {
     override fun createFragment(): Fragment {
         return WelcomeFragment()
     }
