282a7886a05c6b69d57baf8c1ae7dd7b1d8ac66c
Ian Kwiatkowski <iankwiatko@gmail.com>
2024-11-11 19:22:20 -0500

    Routes straight to homepage if user is logged in already

--- app/src/main/java/com/example/schedulemaster/ui/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/example/schedulemaster/ui/fragment/WelcomeFragment.kt
@@ -14,14 +14,19 @@ import com.example.schedulemaster.ui.activity.CalendarActivity
 import com.example.schedulemaster.ui.activity.CreateAccountActivity
 import com.example.schedulemaster.ui.activity.HomeActivity
 import com.example.schedulemaster.ui.activity.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class WelcomeFragment : Fragment(), View.OnClickListener {
+
     private lateinit var mLoginButton: Button
     private lateinit var mCreateAccountButton: Button
-    // below is for testing purposes as of right now
+    // Below is for testing purposes as of right now
     private lateinit var mHomeButton: Button
     private lateinit var mCalendarButton: Button
     private lateinit var mAddTaskButton: Button
+
+    // Firebase Auth instance
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,11 +35,22 @@ class WelcomeFragment : Fragment(), View.OnClickListener {
     ): View {
         val v = inflater.inflate(R.layout.fragment_welcome, container, false)
 
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Check if user is already logged in
+        if (auth.currentUser != null) {
+            val intent = Intent(requireContext(), HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Initialize buttons and set onClick listeners
         mLoginButton = v.findViewById(R.id.loginButton)
         mLoginButton.setOnClickListener(this)
         mCreateAccountButton = v.findViewById(R.id.createAccountButton)
         mCreateAccountButton.setOnClickListener(this)
-        // below is for testing purposes as of right now
+
+        // Buttons for testing purposes
         mHomeButton = v.findViewById(R.id.HomeButton)
         mHomeButton.setOnClickListener(this)
         mCalendarButton = v.findViewById(R.id.CalendarButton)
@@ -49,24 +65,24 @@ class WelcomeFragment : Fragment(), View.OnClickListener {
         when (v.id) {
             R.id.loginButton -> {
                 val intent = Intent(requireContext(), LoginActivity::class.java)
-                startActivity(intent);
+                startActivity(intent)
             }
             R.id.createAccountButton -> {
                 val intent = Intent(requireContext(), CreateAccountActivity::class.java)
-                startActivity(intent);
+                startActivity(intent)
             }
-            // below is for testing purposes as of right now
+            // Below is for testing purposes as of right now
             R.id.HomeButton -> {
                 val intent = Intent(requireContext(), HomeActivity::class.java)
-                startActivity(intent);
+                startActivity(intent)
             }
             R.id.CalendarButton -> {
                 val intent = Intent(requireContext(), CalendarActivity::class.java)
-                startActivity(intent);
+                startActivity(intent)
             }
             R.id.AddTaskButton -> {
                 val intent = Intent(requireContext(), AddTaskActivity::class.java)
-                startActivity(intent);
+                startActivity(intent)
             }
             else -> Log.e("INSIDE WelcomeFragment.kt", "Error: Invalid button press")
         }
