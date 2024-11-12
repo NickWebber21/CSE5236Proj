d4616d501194b4d4993c41e4e9699e6ef61679cd
Ian Kwiatkowski <iankwiatko@gmail.com>
2024-11-11 19:57:54 -0500

    Added routing to new buttons and logout feature works

--- app/src/main/java/com/example/schedulemaster/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/schedulemaster/ui/fragment/HomeFragment.kt
@@ -1,23 +1,55 @@
 package com.example.schedulemaster.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.schedulemaster.R
+import com.example.schedulemaster.ui.activity.AddTaskActivity
+import com.example.schedulemaster.ui.activity.WelcomeActivity
+import com.google.firebase.auth.FirebaseAuth
 
-class HomeFragment : Fragment(), View.OnClickListener  {
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var addTaskButton: Button
+    private lateinit var logoutButton: Button
+
+    // Firebase Auth instance
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        addTaskButton = v.findViewById(R.id.AddTaskButton)
+        addTaskButton.setOnClickListener(this)
+        logoutButton = v.findViewById(R.id.logoutButton)
+        logoutButton.setOnClickListener(this)
+
         return v
     }
 
     override fun onClick(v: View) {
-        TODO("Not yet implemented")
+        when (v.id) {
+            R.id.AddTaskButton -> {
+                val intent = Intent(requireContext(), AddTaskActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.logoutButton -> {
+                auth.signOut()
+                val intent = Intent(requireContext(), WelcomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
