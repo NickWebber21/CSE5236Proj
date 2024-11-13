2744007c114de82a3fba0e1b8ce88176d0f7ad0b
Ian Kwiatkowski <iankwiatko@gmail.com>
2024-11-13 18:19:27 -0500

    updated comments LoginFragment

--- app/src/main/java/com/example/schedulemaster/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/schedulemaster/ui/fragment/LoginFragment.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.schedulemaster.R
-import com.example.schedulemaster.ui.activity.HomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.example.schedulemaster.ui.activity.CalendarActivity
 
@@ -21,12 +20,13 @@ class LoginFragment : Fragment(), View.OnClickListener {
     private lateinit var mEditPasswordText: EditText
     private lateinit var mLoginButton: Button
     private lateinit var auth: FirebaseAuth
-
+    //-----use overloading below-----
+    //executes upon creating the fragment and another instance exists
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
     }
-
+    //executes upon creating the fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,8 +34,7 @@ class LoginFragment : Fragment(), View.OnClickListener {
     ): View {
         val v = inflater.inflate(R.layout.fragment_login, container, false)
         Log.i("LoginFragment", "onCreateView fragment started.")
-
-        // UI element references
+        //bind views
         mEditUsernameText = v.findViewById(R.id.usernameText)
         mEditPasswordText = v.findViewById(R.id.passwordText)
         mLoginButton = v.findViewById(R.id.loginButton)
@@ -59,17 +58,15 @@ class LoginFragment : Fragment(), View.OnClickListener {
             else -> Log.e("LoginFragment", "Error: Invalid button press")
         }
     }
-
+    //signs the user into an account if it exists on firebase auth
     private fun signIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
-                    // Login successful
                     Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
                     val intent = Intent(requireContext(), CalendarActivity::class.java)
                     startActivity(intent)
                 } else {
-                    // Login failed, show error message
                     Log.e("LoginFragment", "Login failed: ${task.exception?.message}")
                     Toast.makeText(requireContext(), "Login failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
